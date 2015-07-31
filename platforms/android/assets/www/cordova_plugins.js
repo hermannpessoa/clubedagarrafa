4fbd5c5b281a6ca7fcdd547fcecdd1a0591035a7
hermannpessoa <performaweb@Performa-Printer.local>
2015-07-31 19:13:51 -0300

    Build Android

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,24 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/com.pushbots.push/www/pushbots.js",
+        "id": "com.pushbots.push.PushbotsPlugin",
+        "clobbers": [
+            "PushbotsPlugin"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.0.0",
+    "com.pushbots.push": "1.2.4",
+    "android.support.v4": "21.0.1"
+}
+// BOTTOM OF METADATA
+});
